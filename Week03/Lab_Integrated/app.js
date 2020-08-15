cf068bf4950a74cc9ac6895ffb4a28bd03b7e136
xaa123aax <52443278+xaa123aax@users.noreply.github.com>
2020-08-15 23:13:55 +0800

    10.改用function形式

--- Week03/Lab_Integrated/app.js
+++ Week03/Lab_Integrated/app.js
@@ -34,8 +34,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host : '127.0.0.1',
 	user : 'root',
-	password : 'root',
-	port:	'8889',
+	password : '',
 	database : 'labdb'
 });
 
